3070e30532092f8b8bb0f0e3338aabcd0b412b05
YO <YO@192.168.0.20>
2019-08-06 03:53:41 -0600

    Initial Commit

---
+++ src/test/java/com/ormg/demo/DemoApplicationTests.java
@@ -0,0 +1,20 @@
+package com.ormg.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.demo.DemoApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+
+}
